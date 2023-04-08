0391f5bbd67b8fa5a9bb570a2b177c2a182b58b2
Yuval <yuvalsharma49@gmail.com>
2023-04-08 23:11:10 +0530

    Adding more programs

---
+++ negativeBinary.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number";
+    cin>>n;
+    n*=-1;
+    while(n != 0){
+        int bits = ~n;
+        
+    }
+}
